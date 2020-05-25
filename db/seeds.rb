067a78ae56f0dd7545f3d39a0eca58a47d2453e6
Philippe Niemann <pniemann@orange.fr>
2020-05-25 20:45:45 +0200

    Start to work on the seed file and I add one meta line for the setup on application.html.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,69 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Ingredient.destroy_all
+
+puts "Start to add Ingredients in our seed"
+
+ingredients = [
+  {
+    name:       'Brocoli',
+    image:      'https://res.cloudinary.com/dubgqyx31/image/upload/w_64,h_48,c_scale/v1590415257/nomess/Brocoli_ywgssh.jpg'
+  }
+  {
+    name:       'Banane',
+    image:      'https://res.cloudinary.com/dubgqyx31/image/upload/w_64,h_48,c_scale/v1590416016/nomess/banane_DAC0XAQ_gpj5to.jpg'
+  }
+  {
+    name:       'Yaourt',
+    image:      'https://res.cloudinary.com/dubgqyx31/image/upload/w_64,h_48,c_scale/v1590417815/nomess/yaourt_fkuzhq.jpg'
+  }
+  {
+    name:       'Parmesan',
+    image:      'https://res.cloudinary.com/dubgqyx31/image/upload/w_64,h_48,c_scale/v1590417470/nomess/parmesan_bj155a.webp'
+  }
+  {
+    name:       'Riz',
+    image:      'https://res.cloudinary.com/dubgqyx31/image/upload/w_64,h_48,c_scale/v1590417749/nomess/riz_fisrkb.jpg'
+  }
+  {
+    name:       'Vin blanc',
+    image:      'https://res.cloudinary.com/dubgqyx31/image/upload/w_64,h_48,c_scale/v1590416386/nomess/sauternes-2016-liquoreux_onhvoe.jpg'
+  }
+  {
+    name:       'Tomate',
+    image:      'https://res.cloudinary.com/dubgqyx31/image/upload/w_64,h_48,c_scale/v1590416469/nomess/tomate_gfkr5j.jpg'
+  }
+  {
+    name:       'Saumon',
+    image:      'https://res.cloudinary.com/dubgqyx31/image/upload/w_64,h_48,c_scale/v1590417300/nomess/saumon_unhydx.png'
+  }
+  {
+    name:       'pâtes',
+    image:      'https://res.cloudinary.com/dubgqyx31/image/upload/w_64,h_48,c_scale/v1590417589/nomess/pates_hzcsf2.png'
+  }
+  {
+    name:       'huile d'olive,
+    image:      'https://res.cloudinary.com/dubgqyx31/image/upload/w_64,h_48,c_scale/v1590416951/nomess/sticker-huile-d-olive_fqxh8w.jpg'
+  }
+  {
+    name:       'poivre',
+    image:      'https://res.cloudinary.com/dubgqyx31/image/upload/w_64,h_48,c_scale/v1590417167/nomess/poivre_fkabwa.webp'
+  }
+]
+
+user = User.create(email:"mess@gmail.com", password:"A12345", first_name: "master", last_name:"chief", address:"nintendoland", phone_number:"0123456789", avatar:"http://www.pngall.com/wp-content/uploads/2/Video-Game-PNG-Pic.png")
+user.save!
+user2 = User.create(email:"messa@gmail.com", password:"A12345", first_name: "yoshi", last_name:"nintendo", address:"nintendoland", phone_number:"0123456788", avatar:"http://www.pngall.com/wp-content/uploads/2/Video-Game-PNG-Pic.png")
+user2.save!
+puts user
+
+ingredients.each do |ingredient|
+  newingredient = Ingredient.new(ingredient)
+  newingredient.user = user
+  newingredient.save!
+end
+
+puts 'Finished!'
