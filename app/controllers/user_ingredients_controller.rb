f7d82b5195486d41dfe23d8638e9ff73e1bb2034
Coralie Delozanne <coralie.delozanne@gmail.com>
2020-05-25 15:51:48 +0200

    worked on new/create functions on the controller

--- app/controllers/user_ingredients_controller.rb
+++ app/controllers/user_ingredients_controller.rb
@@ -6,9 +6,16 @@ class UserIngredientsController < ApplicationController
   end
 
   def new
+    @user_ingredient = UserIngredient.new
   end
 
   def create
+    @user_ingredient = UserIngredient.new(user_ingredients_params)
+    if @user_ingredient.save
+      redirect_to user_ingredients_path
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -18,5 +25,11 @@ class UserIngredientsController < ApplicationController
   end
 
   def delete
+  end
+
+  private
+
+  def user_ingredients_params
+    params.require(:user_ingredient).permit(:quantity, :purchase_date, :expiration_date, :ingredient_id, :user_id)
   end
 end
