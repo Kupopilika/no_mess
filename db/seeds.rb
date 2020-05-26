e71e1498c0ebb6b87c24c18d991c1405f633efe7
Philippe Niemann <pniemann@orange.fr>
2020-05-26 10:34:29 +0200

    update remove user.destroy_all

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.destroy_all
-Ingredient.destroy_all
 
 puts "Start to add Ingredients in our seed"
 
