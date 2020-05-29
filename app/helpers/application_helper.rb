6aa1bc2c1d51c80c356b92efe7fceb9407510b12
Coralie Delozanne <coralie.delozanne@gmail.com>
2020-05-29 14:32:32 +0200

    activenavbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def current_class?(test_path)
+    if request.path == test_path
+      return 'navbar-brand active-perso'
+    else
+      return 'navbar-brand'
+    end
+  end
 end
