9504b8a4b0d34f93ca9c449a778b091b0f235736
Philippe Niemann <pniemann@orange.fr>
2020-06-03 21:26:21 +0200

    create different files for our meta files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
-class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-  def after_sign_in_path_for(resource)
-    user_ingredients_path
-  end
-  def after_sign_up_path_for(resource)
-    user_ingredients
-  end
+# class ApplicationController < ActionController::Base
+#   before_action :authenticate_user!
+#   def after_sign_in_path_for(resource)
+#     user_ingredients_path
+#   end
+#   def after_sign_up_path_for(resource)
+#     user_ingredients
+#   end
+# end
+# app/controllers/application_controller.rb
+
+def default_url_options
+  { host: heroku config:set DOMAIN=www.no-mess.life || "localhost:3000" }
 end
